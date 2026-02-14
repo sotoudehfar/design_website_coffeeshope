02c8f5f6f243ce8bf9fb2fc9dde0c699a06d9302
sotoudehfar <sotoudeh1980@gmail.com>
2026-02-14 22:34:33 +0330

    The Navbar section of the site was designed and implemented in mobile size.

--- script/app.js
+++ script/app.js
@@ -1,10 +1,50 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
-    }
+const submenuOpenBtn = document.querySelector('.submenu-open-btn');
+const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
+const submenu = document.querySelector(".submenu");
+const navOpenBtn = document.querySelector(".navBar");
+const navCloseBtn = document.querySelector(".nav-close-btn");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector('.overlay');
+
+toggleThemeBtns.forEach(btn =>{
+   
+    btn.addEventListener("click", function(){
+        console.log("dobare")
+          if (localStorage.theme === "dark"){
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme" , "dark");
+        }   
+    })
+
+   })
+submenuOpenBtn.addEventListener('click' ,  (e)=>{
+// والد رو نارانجی کن
+       e.currentTarget.parentElement.classList.toggle('text-orange-300');
+//    زیر منوها باز شوند
+       submenu.classList.toggle('submenu--open');
+   })
+
+navOpenBtn.addEventListener('click' ,  ()=>{
+    nav.classList.remove('-right-64');
+    nav.classList.add('right-0');
+    overlay.classList.add('overlay--visible');
+
+   })
+
+navCloseBtn.addEventListener('click', ()=>{
+    nav.classList.add('-right-64');
+    nav.classList.remove('right-0');
+    overlay.classList.remove('overlay--visible');  
+   })
+
+
+overlay.addEventListener('click', ()=>{
+    nav.classList.add('-right-64');
+    nav.classList.remove('right-0');
+    overlay.classList.remove('overlay--visible');     
+
 })
+
